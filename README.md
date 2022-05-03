54f176b47b86392e67b211f97560789c7b0d907d
Luna <lunahatesgogle@gmail.com>
2022-05-03 14:53:06 -0300

    the code may be messy but it does work

---
+++ README.md
@@ -0,0 +1,18 @@
+# update.sh
+A (very bad) auto updater for plugins
+
+## how use
+2. create /scripts/ in server folder and move update.sh there
+1. put stuff in /list.txt
+3. execute /scripts/update.sh
+4. profit?
+
+
+## list.txt format
+`<type> <repo> <cmd> <out> <dest>`  
+example:
+```FETCH	https://ci.plex.us.org/job/Scissors/job/main/lastSuccessfulBuild/artifact/build/libs/Scissors-1.17.1-R0.1-SNAPSHOT.jar										server.jar
+BUILD	git@github.com:AtlasMediaGroup/TotalFreedomMod.git	mvn -B clean package														target/TotalFreedomMod.jar	plugins/TotalFreedomMod.jar
+GHREL	PlayPro/CoreProtect																																			plugins/CoreProtect.jar
+```  
+use tabs not spaces (or else it won't work)
---
+++ update.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+
+SCRIPTSDIR="$(dirname "$0")"
+SERVERDIR="$(dirname "$SCRIPTSDIR")"
+WORKDIR="$SCRIPTSDIR/work"
+
+
+mkdir -p work
+
+build() {
+    NAME=$(basename "$1" .git)
+    OUT=$3
+    DEST=$SERVERDIR/$4
+
+    echo ">>> Building $NAME..."
+
+    if cd "$NAME" 2>/dev/null; then
+        git fetch || return 1
+
+        if [ "$(git rev-parse HEAD)" = "$(git rev-parse "@{u}")" ] && [ -f "$DEST" ]; then
+            # We're already up to date. No need to compile!
+            cd ..
+            echo "### Already up to date."
+            return 0
+        fi
+
+        echo ">>> Fetching $1..."
+        git pull || return 1
+
+    else
+        echo ">>> Fetching $1..."
+        git clone --depth=1 "$1" "$NAME" || return 1
+        cd "$NAME" || return 1
+    fi
+
+    echo ">>> Compiling $NAME..."
+    eval "$cmd" || return 1
+
+    echo ">>> Installing $NAME..."
+
+    # shellcheck disable=SC2086
+    mkdir -p "$(dirname $DEST)" || return 1
+    # shellcheck disable=SC2086
+    cp -v $OUT $DEST
+}
+
+echo "> Updating server files..."
+
+while IFS="$(printf '\t')" read -r type repo cmd out dest; do
+    cd "$WORKDIR" || exit 1
+    
+    if [ "$type" = "BUILD" ]; then
+       if ! build "$repo" "$cmd" "$out" "$dest"; then
+            echo "Got a error while building. Exiting!"
+            exit 1
+        fi
+    elif [ "$type" = "FETCH" ]; then
+        # Binary download
+        echo ">>> Fetching $repo..."
+
+        wget -q -O "$SERVERDIR/$cmd" "$repo" || exit 1
+    elif [ "$type" = "GHREL" ]; then
+        # GitHub release
+        echo ">>> Downloading github release from $repo..."
+
+        URL="$(curl -L "https://api.github.com/repos/$repo/releases/latest" | grep "browser_download_url" | cut -d '"' -f 4)"
+        echo ">>> Fetching $URL..."
+        wget -q -O "$SERVERDIR/$cmd" "$URL" || exit 1
+    fi
+
+    cd "$SCRIPTSDIR" || exit 1
+done < list.txt
+
+echo "> Done updating server files."
